a075f91c6c497a042b6de696cfc17d82e3173067
Owen Harris <harriowe@iu.edu>
2024-04-04 19:02:44 -0400

    added RabbitMQ

--- src/main/java/edu/iu/harriowe/primesservice/controller/PrimesController.java
+++ src/main/java/edu/iu/harriowe/primesservice/controller/PrimesController.java
@@ -1,5 +1,6 @@
 package edu.iu.harriowe.primesservice.controller;
 
+import edu.iu.harriowe.primesservice.rabbitmq.MQSender;
 import edu.iu.harriowe.primesservice.service.IPrimesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,12 +10,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimesController {
     IPrimesService primesService;
-    public PrimesController(IPrimesService primesService) {
+    private final MQSender mqSender;
+    public PrimesController(IPrimesService primesService, MQSender mqSender) {
         this.primesService = primesService;
+        this.mqSender = mqSender;
     }
 
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n) {
-        return primesService.isPrime(n);
+        boolean result = primesService.isPrime(n);
+        mqSender.sendMessage(n, result);
+        return result;
     }
 }
---
+++ src/main/java/edu/iu/harriowe/primesservice/rabbitmq/MQConfiguration.java
@@ -0,0 +1,12 @@
+package edu.iu.harriowe.primesservice.rabbitmq;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+@Configuration
+public class MQConfiguration {
+    @Bean
+    public Queue queue() {
+        return new Queue("primes");
+    }
+}
